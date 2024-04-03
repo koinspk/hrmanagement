bab78f4d36374a9341c51039a94ac3fd7fc8cdfb
DurgaDevikoinnovation <163291318+DurgaDevikoinnovation@users.noreply.github.com>
2024-04-03 18:13:54 +0530

    attendance

--- model/attendanceModel.js
+++ model/attendanceModel.js
@@ -9,4 +9,26 @@ const attendanceSchema = new mongoose.Schema({
 },{ timestamps: true });
 
 
-module.exports =  mongoose.model('employeeSchema',attendanceSchema);
+attendanceSchema.virtual('workingHours').get(function() {
+    if (!this.checkin || !this.checkout) {
+        return null; 
+    }
+
+    const checkinTime = moment(this.checkin);
+    const checkoutTime = moment(this.checkout);
+
+    let breakTimeHours = 0;
+    if (Array.isArray(this.breaktime) && this.breaktime.length >= 2) {
+        for (let i = 0; i < this.breaktime.length - 1; i += 2) {
+            const breakStart = moment(this.breaktime[i], 'HH:mm:ss');
+            const breakEnd = moment(this.breaktime[i + 1], 'HH:mm:ss');
+            const breakDuration = breakEnd.diff(breakStart, 'hours', true);
+            breakTimeHours += breakDuration;
+        }
+    }
+
+    const totalWorkingHours = checkoutTime.diff(checkinTime, 'hours') - breakTimeHours;
+    return totalWorkingHours >= 0 ? totalWorkingHours : 0;
+});
+
+module.exports =  mongoose.model('attendanceSchema',attendanceSchema);
